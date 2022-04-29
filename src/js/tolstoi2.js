f5fcaf4075c73e300463a6d9a9f347111c9af32d
Kevin <playsbykevin@gmail.com>
2022-04-29 19:59:53 +0300

    2 korrus demo

---
+++ src/js/tolstoi2.js
@@ -0,0 +1,11 @@
+const entranceImage = 'assets/Tolstoi/2.korrus/temp.jpg'
+const entrancePano = new PANOLENS.ImagePanorama(entranceImage)
+const viewer = new PANOLENS.Viewer({
+    output: 'console',
+});
+
+infospot2 = new PANOLENS.Infospot();
+infospot2.position.set( -5000.00, -1825.25, 197.56 );
+infospot2.addHoverElement(document.getElementById('desc-container'), 200);
+entrancePano.add(infospot2);
+viewer.add(entrancePano);
