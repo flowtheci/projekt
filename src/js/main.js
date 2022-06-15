7bfb14f01311ec779a29cb3d5fa42a96f136b659
flowtheci <kevinerik.iin@gmail.com>
2022-06-15 14:17:01 +0300

    Merged fork

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,5 @@ customInfo.position.set(xCoord, yCoord, zCoord);
 customInfo.addHoverElement(document.getElementById(elementId), 200);
 }
 
-createInfoSpot(pano, -2151.91, 911.23, -4409.72, 'desc-container');
+
+createInfoSpot(pano, 4892.02, 117.66, -1001.94, 'desc-container');
